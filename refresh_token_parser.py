66d6ddb11da7c1514acd69df04f79bdc9b07b532
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-10-26 14:53:36 -0400

    Added type hinting with fallback if mitmproxy package isn't available

--- refresh_token_parser.py
+++ refresh_token_parser.py
@@ -1,3 +1,14 @@
+# Author: Quintin Dunn
+# Date: 10/26/23
+
+# Import mitmproxy for type hinting, but if it cannot find mitmproxy just use the str "HTTPFlow" as it has no functional
+# difference
+try:
+    from mitmproxy.http import HTTPFlow
+except ImportError:
+    HTTPFlow = "HTTPFlow"
+
+
 def log_output(access_token: str, refresh_token: str, user_id: str):
     print(f"Access Token: {access_token}")
     print(f"Refresh Token: {refresh_token}")
@@ -10,7 +21,9 @@ class ParseRefreshToken:
         self.refresh_token: str | None = None
         self.user_id: str | None = None
 
-    def response(self, flow):
+    def response(self, flow: HTTPFlow):
+        print(flow.client_conn.address)
+
         if flow.request.url == "https://auth.production.journal-api.lapse.app/verify" \
                 and flow.response.status_code == 200:
             json = flow.response.json()
