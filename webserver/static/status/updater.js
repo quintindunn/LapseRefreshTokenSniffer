49084e9fb7f1b4a6a004e2d006144cbd1b6e9887
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-10-29 18:28:52 -0400

    Removed extraneous passthroughs to js

--- webserver/static/status/updater.js
+++ webserver/static/status/updater.js
@@ -1,5 +1,3 @@
-const proxy_ip = document.getElementById("p-ip").value;
-const proxy_port = document.getElementById("p-port").value;
 const proxy_username = document.getElementById("p-username").value;
 const proxy_password = document.getElementById("p-password").value;
 
